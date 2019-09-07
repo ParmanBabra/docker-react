eee4eacc3540c38b8625c06a1b32670f0dee6218
DESKTOP-PT7T5ME\Parman-PC2 <ParmanbAbra@hotmail.com>
2019-09-08 01:10:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!
+          Testing
         </p>
         <a
           className="App-link"
